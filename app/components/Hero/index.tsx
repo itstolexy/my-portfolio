6d84a0ebc168f7c034006ca0b955f2efe948a5d3
Omotola Odusanya <odusanyaomotola64@gmail.com>
2026-02-16 07:01:24 +0100

    chore: updated the view cv drive link

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -172,7 +172,7 @@ const Hero = () => {
           icon={<InfoOutlinedIcon fontSize="large" />}
         /> */}
         <Button
-          href="https://drive.google.com/file/d/1C17sJkxw05dwwmXoy9TLl3J8shpu-n6O/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1mtvg-70SoabH8TIfd8L-2sR7dQWO6AgC/view?usp=drivesdk"
           text="VIEW RESUME"
           icon={<DownloadForOfflineOutlinedIcon fontSize="large" />}
           target="_blank"
